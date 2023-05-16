1008021fb95f503d3e3cc57d2b47fe3721943ec7
hiwina <hiwina@gmail.com>
2023-05-16 14:08:38 +0300

    configure linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode:'development',
-    entry: {
-        bundel:path.resolve(__dirname, 'src/index.js'),
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
+  mode: 'development',
+  entry: {
+    bundel: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/i,
@@ -20,7 +20,7 @@ module.exports = {
       },
     ],
   },
-   plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack App',
       filename: 'index.html',
