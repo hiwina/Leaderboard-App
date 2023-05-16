540b408ad1f97bd5ce64e7f83b077a12187d9035
hiwina <hiwina@gmail.com>
2023-05-16 13:21:13 +0300

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode:'development',
+    entry: {
+        bundel:path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader',
+        ],
+      },
+    ],
+  },
+   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+};
